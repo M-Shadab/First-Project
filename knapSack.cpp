8693bc1ba074ff0168b9bc2f33c839288050fbd5
M-Shadab <shadab.saifi007@gmail.com>
2018-08-16 00:12:23 +0530

    Adding first Project

---
+++ knapSack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//0-1 Knapsack :: Pick(=1)  and Not pick(=0)
+int knapsack(int value[], int weight[], int size, int wt, int val){
+	if(size == 0 || wt == 0)	return val;
+
+	//1. Pick 0th value if possible 1: case
+	int ans1 = INT_MIN;
+	if(wt - weight[0] >= 0){
+		ans1 = knapsack(value + 1, weight + 1, size - 1, wt - weight[0], val + value[0]);
+	}
+
+	//2. don't pick 0:Case
+	int ans2 = knapsack(value + 1, weight + 1, size - 1, wt, val);
+
+	return max(ans1, ans2);
+}
+
+int main(){
+	int value[] = {60, 100, 120};
+	int weight[] = {10, 20, 30};
+	int wt = 50;
+
+	int maxValue = knapsack(value, weight, 3, wt, 0);
+	
+	cout << maxValue << endl;
+	
+	return 0;
+}
